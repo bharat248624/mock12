07756a21082438963c664475e01244f39ed985dc
Bharatsharma <108224876+bharat24862486@users.noreply.github.com>
2023-06-13 14:40:55 +0530

    mock-12

--- routes/job.route.js
+++ routes/job.route.js
@@ -6,35 +6,27 @@ const jobRoute = express.Router()
 
 jobRoute.get('/', async (req, res) => {
     let { role, date, techstack,page=1 } = req.query
+    let obj={}
     let Limit = 10
     console.log(date, role, techstack)
     // console.log(query)
     try {
 
-        if (!role && !date && !techstack) {
-            let data = await jobModel.find().limit(10).skip((page-1)*Limit)
-            res.send(data)
-        }
+        
 
         if (role) {
-            let data = await jobModel.find({ role: role }).limit(10).skip((page-1)*Limit)
-            res.send(data)
+            obj.role = role
         }
         if (techstack) {
-            console.log('hello')
-            let data = await jobModel.find({ language: techstack }).limit(10).skip((page-1)*Limit)
-            res.send(data)
+           obj.language=techstack
+        }
+
+        if(page <= 0){
+            page = 1
         }
         // db.coll.find({$text: {$search: "cake"}}, {score: {$meta: "textScore"}}).sort({score: {$meta: "textScore"}})
-        if (date) {
-            if (date == 'asc') {
-                let data = await jobModel.find().sort({'postedAt':+1}).limit(10).skip((page-1)*Limit)
-                res.send(data)
-            } else {
-                let data = await jobModel.find().sort({'postedAt':-1}).limit(10).skip((page-1)*Limit)
-                res.send(data)
-            }
-        }
+        let data = await jobModel.find(obj).limit(10).skip((page-1)*Limit)
+            res.send(data)
 
 
     } catch (error) {
